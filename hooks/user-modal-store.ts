0c0e3e03b4932b08363b82d999fc7e35ff7f10c8
vikrant845 <vikrantswaron@gmail.com>
2023-12-03 17:58:32 +0530

    Fixed Type On useModal

--- hooks/user-modal-store.ts
+++ hooks/user-modal-store.ts
@@ -5,7 +5,7 @@ import { create } from 'zustand';
 export type ModalType = 'createServer' | 'invite' | 'editServer' | 'members' | 'createChannel' | 'leaveServer' | 'deleteServer' | 'deleteChannel' | 'editChannel' | 'messageFile' | 'deleteMessage';
 
 interface ModalData {
-    server?: ServerWithMembersWithProfiles | null;
+    server?: ServerWithMembersWithProfiles & Server | null;
     channel?: Channel;
     channelType?: ChannelType;
     apiUrl?: string;
